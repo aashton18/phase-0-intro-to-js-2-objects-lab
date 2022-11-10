8b98af5d24df6450782b8672b83e11deec3a6bd1
april <aprilashton18@gmail.com>
2022-11-10 04:39:20 -0800

    commit tree 58f1f23070dc6ac60fab71d945868ce1ceec1378

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+  name: 'April',
+  address: '994 Monterey Way',
+}
+
+// Task 1
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObject = {...employee};
+  newObject.key = value;
+  const getValues = [employee, newObject]
+  return `getValues`;
+}
+
+//Task 2
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = [value];
+  return `employee`;
+}
+
+//Task 3
+function deleteFromEmployeeByKey(employee, key) {
+  const newObject = {...employee};
+  delete newObject[key];
+  return newObject;
+}
+
+//Task 4
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return `employee`;
+}
